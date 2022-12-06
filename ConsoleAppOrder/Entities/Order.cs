8cde514b00d2c5d5423da2e35ed43e652da532d4
Alexandre Conte <alexandre.conte10@gmail.com>
2022-12-06 13:48:52 -0300

    Commit

---
+++ ConsoleAppOrder/Entities/Order.cs
@@ -0,0 +1,58 @@
+﻿using ConsoleAppOrder.Entities.Enums;
+using System.Globalization;
+using System.Text;
+
+namespace ConsoleAppOrder.Entities
+{
+    internal class Order
+    {
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+        public Client Client { get; set; }
+        public List<OrderItem> Items { get; set; } = new List<OrderItem>();
+
+        public Order() { }
+
+        public Order(DateTime date, OrderStatus status, Client client)
+        {
+            Moment = date;
+            Status = status;
+            Client = client;
+        }
+        
+        public void AddItem(OrderItem item)
+        {
+            Items.Add(item);
+        }
+
+        public void RemoveItem(OrderItem item)
+        {
+            Items.Remove(item);
+        }
+
+        public double Total()
+        {
+            double sum = 0;
+            foreach (OrderItem item in Items)
+            {
+                sum += item.subTotal();
+            }
+            return sum;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("Order moment: " + Moment.ToString("dd/MM/yyyy HH:mm:ss"));
+            sb.AppendLine("Order Status: " + Status);
+            sb.AppendLine("Client: " + Client);
+            sb.AppendLine("Order Items:");
+            foreach (OrderItem item in Items)
+            {
+                sb.AppendLine(item.ToString());
+            }
+            sb.AppendLine("Total price: $" + Total().ToString("F2", CultureInfo.InvariantCulture));
+            return sb.ToString();
+        }
+    }
+}
